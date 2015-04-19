9106d9a1bda3d7a3a747f52548472b0823785c46
JJLim <lim.jess9@gmail.com>
2015-04-19 00:52:20 -0400

    Create and route login feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
   resources :users
+  resources :sessions
+  
+  get 'login' => 'sessions#new', :as => :login
+  post 'logout' => 'sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
